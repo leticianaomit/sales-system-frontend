f4954d50b284de11b55be3882c060afdde29b4c4
Letícia Naomi Takeshitro <leticiatakeshiro@hotmail.com>
2021-10-31 04:04:51 -0300

    style: toggle show toolbar and sidenav

--- src/app/shared/layout/components/toolbar/toolbar.component.ts
+++ src/app/shared/layout/components/toolbar/toolbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  @Output() toggleSidenav = new EventEmitter<boolean>();
+  
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClickBtnShowSidenav() {
+    this.toggleSidenav.emit(true);
   }
-
 }
--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  navMode!: MatDrawer['mode'];
+  showNav: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.setNavMode(window.innerWidth);
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setNavMode(window.innerWidth);
+  }
+
+  setNavMode(screenWidth: number) {
+    if (screenWidth > 1280) {
+      this.navMode = 'side';
+      this.showNav = true;
+    } else {
+      this.navMode = 'over';
+      this.showNav = false;
+    }
+  }
 }
